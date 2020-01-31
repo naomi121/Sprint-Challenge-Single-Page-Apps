43c2153831f6ae24882b903aaa8bf5cb2e7d0e77
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-31 11:26:53 -0500

    added imports to App.js in lines 3-5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
+import { Route } from "react-router-dom";
+
 
 
 export default function App() {
